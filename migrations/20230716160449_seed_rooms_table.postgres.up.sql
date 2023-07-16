8e73685fb1d7e291668524c357df9d87e74331a0
lysenkopavlo <79150347007@ya.ru>
2023-07-16 19:09:35 +0300

    Added seeds for rooms and restrictions

---
+++ migrations/20230716160449_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+	 ('General''s Quarters','2023-07-07 00:00:00.000','2023-07-07 00:00:00.000'),
+	 ('Major''s suite','2023-07-10 00:00:00.000','2023-07-10 00:00:00.000');
---
+++ migrations/20230716160704_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name,created_at,updated_at) VALUES
+	 ('Reservation','2023-07-08 00:00:00.000','2023-07-08 00:00:00.000'),
+	 ('Owner''s block','2023-07-10 00:00:00.000','2023-07-10 00:00:00.000');
